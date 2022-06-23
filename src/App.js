5ec6f70d81b4991a0219831f50051acd102fcc89
aditya-chari25 <adi25charis@gmail.com>
2022-06-23 15:06:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import Axios from "axios";
 
 function App() {
+  const[listTasks,setListTasks] = useState([]);
+  const[name,setName] = useState("")
+  const[taskname,setTaskname]=useState("")
+
+  useEffect(()=>{
+    Axios.get("https://mern-prc.herokuapp.com/getTask")
+    .then((res)=>{
+      setListTasks(res.data)
+    })
+  },[])
+
+  const createTask =()=>{
+    Axios.post("https://mern-prc.herokuapp.com/createTask",{
+      name:name,
+      task:taskname,
+    })
+    .then((res)=>{
+      Axios.get("https://mern-prc.herokuapp.com/getTask")
+    .then((res)=>{
+      setListTasks(res.data)
+    })
+    });
+  }
+
+  const deleteTask =(ob1,ob2)=>{
+    Axios.post("https://mern-prc.herokuapp.com/deleteTask",{
+      ob1,
+      ob2,
+    })
+    .then((res)=>{
+      Axios.get("https://mern-prc.herokuapp.com/getTask")
+    .then((res)=>{
+      setListTasks(res.data)
+    })
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tasks">
+        {listTasks.map((task)=>{
+          return(
+            <div>
+              <h1>Name:{task.name}</h1>
+              <h1>Task:{task.task}</h1>
+              <button onClick={()=>deleteTask(task.name,task.task)}>Delete Task</button>
+              <h3>----------------------------------------</h3>
+            </div>
+          )
+        })}
+      </div>
+
+      <div>
+        <input type="text" placeholder="Name" onChange={(event)=>{
+          setName(event.target.value);
+        }}/>
+        <input type="text" placeholder="Task" onChange={(event)=>{
+          setTaskname(event.target.value);
+        }}/>
+        <button onClick={createTask}>Create Task</button>
+      </div>
+
     </div>
+
+
   );
 }
 
